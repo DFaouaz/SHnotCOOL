c65e64e253584e76e2c4613ab95b495255797e68
DFaouaz <36241623+DFaouaz@users.noreply.github.com>
2018-03-30 20:24:31 +0200

    Bocetado y funcionalidad básica del minijuego de Lengua

---
+++ SHnotCOOL/Assets/Scripts/Lengua/Letter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Letter : MonoBehaviour {
+
+	public char letra;
+	TextMesh caracter;
+
+	void Start(){
+		caracter = GetComponent<TextMesh>();
+		caracter.text = letra.ToString();
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+		if (col.tag == "DeathZone")
+			Destroy (this.gameObject);
+	}
+
+}
