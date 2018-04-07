ff16f84d9285ae46ba25b579ed0a18a950337029
AlansonUCM <37536373+AlansonUCM@users.noreply.github.com>
2018-04-07 13:40:38 +0200

    Respuestas al azar
    Respuestas al azar en los botones dos buenas y dos malas para cambiarlas modificar archivos txt

---
+++ SHnotCOOL/Assets/Scripts/Historia/AnswerManager.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Text;
+
+public struct opcion{
+	public string frase;
+	public int valor;
+}
+
+public class AnswerManager : MonoBehaviour {
+
+	public static AnswerManager instance = null;
+
+	opcion[] respuestas;
+	List<opcion> opciones;
+	opcion [] Buenas;
+	opcion [] Malas;
+	bool pulsado;
+
+	void Start () {
+		if (instance == null)
+			instance = this;
+	
+	     
+		pulsado = false;
+		StreamReader texto = new StreamReader("buena.txt", Encoding.Default);
+		StreamReader texto2 = new StreamReader("mala.txt", Encoding.Default);
+		opciones = new List<opcion> ();
+		Buenas = new opcion[8];
+		Malas = new opcion[8];
+		respuestas = new opcion[4];
+		int i = 0;
+		while (i < Buenas.Length) {
+			Buenas [i].valor = texto.Read ();
+			Buenas [i].frase = texto.ReadLine ();
+			Malas [i].valor = texto2.Read ();
+			Malas [i].frase = texto2.ReadLine ();
+			i++;
+		}
+		InitButtons ();
+	}
+	
+
+	public void InitButtons()
+	{
+
+		int x, y, w, z;
+		x = Random.Range (0, Buenas.Length);
+		y = Random.Range (0, Buenas.Length);
+		w = Random.Range (0, Malas.Length);
+		z = Random.Range (0,Malas.Length);
+
+		while(x==y)
+			y = Random.Range (0, Buenas.Length);
+		while(w==z)
+			z = Random.Range (0,Malas.Length);
+
+		opciones.Add (Buenas [x]);
+		opciones.Add (Buenas [y]);
+		opciones.Add (Malas [w]);
+		opciones.Add (Malas [z]);
+		int contador = 0;
+		while (opciones.Count > 0) {
+			int posicion = Random.Range (0, opciones.Count);
+			respuestas[contador]=opciones [posicion];
+			opciones.RemoveAt (posicion);
+			contador++;
+		}
+
+	}
+
+	public opcion getOpcion(int index)
+	{
+		return respuestas [index];
+	}
+
+	public bool getPulsado(){
+		return pulsado;
+	}
+	public void setPulsado(bool ok)
+	{
+		pulsado = ok;
+	}
+}
---
+++ SHnotCOOL/Assets/Scripts/Historia/boton.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class boton : MonoBehaviour {
+	public int index;
+	opcion MiOpcion;
+	bool turno;
+	int valor;
+
+	Text texto;
+	void Start () {
+		
+		texto = GetComponentInChildren<Text> ();
+		AsignarOpcion ();
+		turno = false;
+	}
+
+
+	void Update()
+	{
+		if (!turno) {
+			if (AnswerManager.instance.getPulsado ())
+				AsignarOpcion ();
+		}
+		else {
+			AnswerManager.instance.setPulsado (false);
+			turno = false;
+		}
+	}
+
+	void AsignarOpcion()
+	{
+		MiOpcion = AnswerManager.instance.getOpcion (index);
+		texto.text = MiOpcion.frase;
+		valor = MiOpcion.valor;
+
+	}
+	public void clikado()
+	{
+		AnswerManager.instance.setPulsado (true);
+		AnswerManager.instance.InitButtons ();
+		turno = true;
+		AsignarOpcion ();
+	}
+}
