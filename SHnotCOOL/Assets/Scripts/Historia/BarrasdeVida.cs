78483da18f27fc8f48c47acd211bcd8fea3e1ad5
apoyatos <37510165+apoyatos@users.noreply.github.com>
2018-04-07 13:08:53 +0200

    Añadidas las barras de vida
    Añadidas las barras de vida y la forma de aumentarlas y disminuirlas

---
+++ SHnotCOOL/Assets/Scripts/Historia/BarrasdeVida.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarrasdeVida : MonoBehaviour {
+    float Health=100;
+	// Use this for initialization
+	void Start () {
+        
+            DisminuyeVida();
+     
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        ActualizaBarra();
+    }
+
+    void DisminuyeVida()
+    {
+        if(Health>0)
+            Health-= 10f;
+        
+    }
+    void AumentaVida()
+    {
+        if (Health <100)
+            Health += 1f;
+    }
+    void ActualizaBarra()
+    {
+        gameObject.GetComponent<Slider>().value =Health;
+    }
+}
