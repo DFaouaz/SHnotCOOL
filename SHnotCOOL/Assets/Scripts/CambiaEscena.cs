7b307210a97b3c954fdf4f59d81d0d42669be6dd
AlansonUCM <37536373+AlansonUCM@users.noreply.github.com>
2018-04-12 22:56:39 +0200

    2nd intento de arreglo de conflicto
    Resolucion de errores en el commit

--- SHnotCOOL/Assets/Scripts/CambiaEscena.cs
+++ SHnotCOOL/Assets/Scripts/CambiaEscena.cs
@@ -8,10 +8,9 @@ public class CambiaEscena : MonoBehaviour {
 	public string escenaACambiar;
 	void OnTriggerEnter2D(Collider2D col){
 		Debug.Log ("Entrada");
-		if (col.tag == "Player" && (escenaACambiar == "Matematicas" && GameManager.instance.matematicasScore == 0)|| (escenaACambiar == "Lengua" && GameManager.instance.lenguaScore == 0) || (escenaACambiar == "Historia" && GameManager.instance.historiaScore == 0) ) {		//ES MA PROVISIONAL
+		if (col.tag == "Player" && GameManager.instance.matematicasScore == 0) {		//ES MA PROVISIONAL
 			GameManager.instance.Escena1PlayerPos = GameManager.instance.ActualPlayerPosition;
 			SceneManager.LoadScene (escenaACambiar);
 		}
 	}
-    
 }
