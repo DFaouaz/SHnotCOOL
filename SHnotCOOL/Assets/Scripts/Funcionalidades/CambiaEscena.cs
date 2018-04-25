a34ba890bd6ccb0edde3a16a104f90ec971e0578
apoyatos <37510165+apoyatos@users.noreply.github.com>
2018-04-25 11:22:36 +0200

    Arreglado la opcion de cambio de escena
    arreglado el script de cambio de escena

--- SHnotCOOL/Assets/Scripts/Funcionalidades/CambiaEscena.cs
+++ SHnotCOOL/Assets/Scripts/Funcionalidades/CambiaEscena.cs
@@ -9,49 +9,34 @@ public class CambiaEscena : MonoBehaviour {
     Text mensajeEscena;
 	public string escenaACambiar;// Aula si es un examen
     public string Examen;
+    bool colision;
+    bool pasillos;
     private void Start()
     {
         mensajeEscena = GameObject.FindGameObjectWithTag("MensajeEscena").GetComponent<Text>();
     }
     void OnTriggerEnter2D(Collider2D col){
 
+
+
         if (col.tag == "Player")
         {
-            if (escenaACambiar == "Aula")
-            {
-                int num = Random.Range(0, 10);
-                if (num < 3)
-                    SceneManager.LoadScene("Pasillos");
-                else
-                {
-                    mensajeEscena.gameObject.SetActive(true);
-                    if (escenaACambiar != "Aula")
-                        mensajeEscena.text = "Pulsa X para acceder al " + escenaACambiar;
-                    else
-                        mensajeEscena.text = "Pulsa X para acceder al Aula de " + Examen;
-                    if (Input.GetKeyDown(KeyCode.X))
-                    {
-                        mensajeEscena.gameObject.SetActive(false);
-                        SceneManager.LoadScene(escenaACambiar);
-                    }
-                }
-            }
+            colision = true;
+            mensajeEscena.gameObject.SetActive(true);
+            if (escenaACambiar != "Aula")
+                mensajeEscena.text = "Pulsa X para acceder al " + escenaACambiar;
             else
-            {
-                mensajeEscena.gameObject.SetActive(true);
-                if (escenaACambiar != "Aula")
-                    mensajeEscena.text = "Pulsa X para acceder al " + escenaACambiar;
-                else
-                    mensajeEscena.text = "Pulsa X para acceder al Aula de " + Examen;
-                if (Input.GetKeyDown(KeyCode.X))
+                mensajeEscena.text = "Pulsa X para acceder al Aula de " + Examen;
+                if (escenaACambiar == "Aula")
                 {
-                    mensajeEscena.gameObject.SetActive(false);
-                    SceneManager.LoadScene(escenaACambiar);
+                    int num = Random.Range(0, 10);
+                    if (num < 3)
+                        pasillos = true;                        
                 }
-            }
+           
                 if (escenaACambiar != "Piso1")
                     GameManager.instance.Escena1PlayerPos = GameManager.instance.ActualPlayerPosition;
-            
+
                 if (Examen == "Matematicas" && !GameManager.instance.finMates)
                     GameManager.instance.Examen = 0;
                 else if (Examen == "Historia" && !GameManager.instance.finHistoria)
@@ -63,6 +48,24 @@ public class CambiaEscena : MonoBehaviour {
                 else
                     GameManager.instance.Examen = 4;
             }
+        
         }
-	}
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        mensajeEscena.gameObject.SetActive(false);
+        colision = false;
+    }
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.X)&&colision)
+        {
+            if (pasillos)
+                SceneManager.LoadScene("Pasillos");
+            else
+                SceneManager.LoadScene(escenaACambiar);
+            mensajeEscena.text = "";
+        }
+    }
+
+}
 
