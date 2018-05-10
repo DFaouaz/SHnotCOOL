49fedd12127b257b89624a6d959fead6e85cdd1e
IsmaFern <IsmaFern@users.noreply.github.com>
2018-05-10 20:03:51 +0200

    Añadido panel de información en minijuegos

---
+++ SHnotCOOL/Assets/Scripts/Funcionalidades/InfoWindow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InfoWindow : MonoBehaviour
+{
+    public GameObject mesagePanel;
+    public Text text;
+    public string mesage;
+
+	void Awake ()
+    {
+        Time.timeScale = 0;
+	}
+
+	void Start ()
+    {
+        text.text = mesage;
+	}
+
+    public void ClosePanel()
+    {
+        mesagePanel.SetActive(false);
+        Time.timeScale = 1;
+    }
+}
