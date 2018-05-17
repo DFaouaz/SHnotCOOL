5808a03db7b78e3eec64f601de058a05772b4f42
IsmaFern <IsmaFern@users.noreply.github.com>
2018-05-17 20:37:18 +0200

    Añadidos paneles de información

---
+++ SHnotCOOL/Assets/Scripts/Funcionalidades/InfoPanel.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InfoPanel : MonoBehaviour
+{
+    public GameObject mesagePanel;
+    public Text text;
+    public string mesage;
+    bool active = true;
+
+    void Awake()
+    {
+        if (active)
+        {
+            OpenPanel();
+        }
+    }
+
+    void OpenPanel()
+    {
+        text.text = mesage;
+        mesagePanel.SetActive(active);
+        Time.timeScale = 0;
+    }
+
+    public void ClosePanel()
+    {
+        mesagePanel.SetActive(!active);
+        Time.timeScale = 1;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            OpenPanel();
+            Destroy(this);
+        }
+    }
+}
