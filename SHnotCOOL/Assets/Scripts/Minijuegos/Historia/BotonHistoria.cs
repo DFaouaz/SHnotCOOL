8bfe668c13c50afd6964437fa92a6d072d48642c
DFaouaz <dfaouaz@ucm.es>
2018-05-28 00:02:45 +0200

    Cambios en nombres de las clases y demás

---
+++ SHnotCOOL/Assets/Scripts/Minijuegos/Historia/BotonHistoria.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BotonHistoria : MonoBehaviour {
+
+    public int index;
+    Opcion miOpcion;
+    Text text;
+    int value;
+
+    void Start() {
+
+        text = GetComponentInChildren<Text>();
+        AsignarOpcion();
+    }
+
+    void Update() {
+
+        if (!GameManager.instance.pauseMode && !GameManager.instance.ventanaAbierta)
+            AsignarOpcion();
+    }
+
+    void AsignarOpcion()
+    {
+        miOpcion = AnswerManager.instance.GetOpcion(index);
+        text.text = miOpcion.frase;
+        value = miOpcion.valor;
+    }
+
+    public void Clickado()
+    {
+        AnswerManager.instance.SetDamage(value);
+        AnswerManager.instance.SetPulsado(true);
+        AnswerManager.instance.InitButtons();
+        AsignarOpcion();
+    }
+}
---
+++ SHnotCOOL/Assets/Scripts/Minijuegos/Maton/MatonBehaivour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MatonBehaivour : MonoBehaviour {
+
+	public int probInicial, tiempo;
+
+	void Start () {
+
+		Invoke("ProMaton", tiempo);
+	}
+	
+    void ProMaton()
+    {
+        int prob = Random.Range(0, 100);
+
+		if (prob < (probInicial - ((GameManager.instance.numFriends * probInicial) / GameManager.instance.maxFriends)) && GameManager.instance.matonAble 
+            && !GameManager.instance.pauseMode && !GameManager.instance.ventanaAbierta)
+        {
+			GameManager.instance.lastPosPasillos = GameObject.FindGameObjectWithTag ("Player").transform.position;
+			GameManager.instance.lastPosEntrance = Vector3.zero;
+            SceneManager.LoadScene("Maton");
+        }
+        Invoke("ProMaton", tiempo);
+    }
+}
